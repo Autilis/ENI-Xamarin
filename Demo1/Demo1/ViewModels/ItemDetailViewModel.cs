f5b39f58221db816c700435f82d59b7aaef95dd0
Matthieu DE LAJARTE <matthieu.delajarte2020@campus-eni.fr>
2020-09-29 10:19:34 +0200

    tp1

---
+++ Demo1/Demo1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Demo1.Models;
+
+namespace Demo1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
